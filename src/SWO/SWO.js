389af4aee4b6cd7ffa8a2ce73aa55909cccec466
Sai-git1A <saidharshan018@gmail.com>
2023-08-08 14:33:14 +0530

    SWO Page

--- src/SWO/SWO.js
+++ src/SWO/SWO.js
@@ -7,15 +7,28 @@ export default function SWO() {
     return (
         <>
            <Navbar data={cart}/>
-           <div className='list'>
-            <ul typeof='circle'>
-                <li>Landscape designing</li>
-                <li>Gardening Services</li>
-                <li>Contract Growing</li>
-                <li>Planting Services</li>
-                <li>Horticultural Services</li>
-            </ul>
-           </div> 
+           <div className='swo-list'>
+            <div className='swo-item'>
+                <img className='swo-item-img' src='https://res.cloudinary.com/safedb/image/upload/v1691481994/KK%20Nursery/Services%20We%20Offer/Landscape-Designing_lid6i6.jpg' alt='Landscape-Designing' loading='lazy'/>
+                <h1 className='swo-item-title'>Landscape Designing</h1>
+            </div>
+            <div className='swo-item'>
+                <img className='swo-item-img' src='https://res.cloudinary.com/safedb/image/upload/v1691481993/KK%20Nursery/Services%20We%20Offer/Gardening-Services_fuvpwj.jpg' alt='Landscape-Designing' loading='lazy'/>
+                <h1 className='swo-item-title'>Gardening Services</h1>
+            </div>
+            <div className='swo-item'>
+                <img className='swo-item-img' src='https://res.cloudinary.com/safedb/image/upload/v1691481993/KK%20Nursery/Services%20We%20Offer/Planting-Services_lrpco5.jpg' alt='Landscape-Designing' loading='lazy'/>
+                <h1 className='swo-item-title'> Planting Services</h1>
+            </div>
+            <div className='swo-item'>
+                <img className='swo-item-img' src='https://res.cloudinary.com/safedb/image/upload/v1691481993/KK%20Nursery/Services%20We%20Offer/Contract-Farming_tzbomr.jpg' alt='Landscape-Designing' loading='lazy'/>
+                <h1 className='swo-item-title'>Contract Farming</h1>
+            </div>
+            <div className='swo-item'>
+                <img className='swo-item-img' src='https://res.cloudinary.com/safedb/image/upload/v1691481993/KK%20Nursery/Services%20We%20Offer/Horticulture_ry7vup.jpg' alt='Landscape-Designing' loading='lazy'/>
+                <h1 className='swo-item-title'>Horticulture Services</h1>
+            </div>
+           </div>
         </>
     )
 }
