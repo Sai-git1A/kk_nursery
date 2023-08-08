efa138dd6b4c343da0fb0d7d060cc5161d7d2290
Sai-git1A <saidharshan018@gmail.com>
2023-08-08 13:32:15 +0530

    Contact Us Dialog

--- src/Components/Dialog/Dialog.js
+++ src/Components/Dialog/Dialog.js
@@ -16,11 +16,11 @@ export default function Dialog({onClick}) {
                 <div className="contact-us-links">
                 <div className="contact-us-link-1">
                 <i className="fa-solid fa-phone"></i>
-                <span className='contact-us-link'> +91-7386-7220-20</span>
+                <a className='contact-us-link' href='tel:+917386722020'> +91-7386-7220-20</a>
                 </div>
                 <div className="contact-us-link-2">
                 <i className="fa-solid fa-envelope"></i>
-                <span className="contact-us-link"> srikrishnakumariplantsnursery@gmail.com</span>
+                <a className="contact-us-link" href='mailto:kknurseries.official@gmail.com'> kknurseries.official@gmail.com</a>
                 </div>
                 <div className="contact-us-link-3">
                 <i className="fa-solid fa-globe"></i>
