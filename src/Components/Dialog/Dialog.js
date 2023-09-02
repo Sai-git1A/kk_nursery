18744400539ff95a3a600b51714689b5b54c1404
Sai Dharshan <saidharshan018@gmail.com>
2023-09-02 14:57:09 +0530

    Contact Us Dialog Update

--- src/Components/Dialog/Dialog.js
+++ src/Components/Dialog/Dialog.js
@@ -9,22 +9,18 @@ export default function Dialog({onClick}) {
         <>
             <div className='main'>
                 <div className='content'>
-                <div className='contact-us-title-div'>
-                <h1 className='contact-us-title'>Contact Us</h1>
-                <button className='btn' onClick={() => handelClick()}><i className="fa-solid fa-xmark"></i></button>
-                </div>
-                <div className="contact-us-links">
+                <button className='btn-close' onClick={() => handelClick()}><i className="fa-solid fa-xmark"></i></button>
+                <div className="contact-us-content">
+                <h1 className='contact-us-title'>Ramana Naidu</h1>
+                <span className='contact-us-subtitle'>Owner</span>
                 <div className="contact-us-link-1">
-                <i className="fa-solid fa-phone"></i>
-                <a className='contact-us-link' href='tel:+917386722020'> +91-7386-7220-20</a>
+                <span className='contact-us-info'>+91-7386-7220-20</span>
                 </div>
-                <div className="contact-us-link-2">
-                <i className="fa-solid fa-envelope"></i>
-                <a className="contact-us-link" href='mailto:kknurseries.official@gmail.com'> kknurseries.official@gmail.com</a>
+                <div className="contact-us-link-div-2">
+                <span className='contact-us-info'>kknurseries.official@gmail.com</span>
                 </div>
                 <div className="contact-us-link-3">
-                <i className="fa-solid fa-globe"></i>
-                <span className="contact-us-link"> kknurseries.com</span>
+                <span className="contact-us-info"> kknurseries.com</span>
                 </div>
                 </div>
                 </div>
