e5a803a893f4e3f7b8ab239e8222a55fe84fb954
Sai Dharshan <saidharshan018@gmail.com>
2023-10-15 13:19:54 +0530

    Style Change for Policy Pages

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Navbar from '../Components/Nav/Navbar';
 import Footer from '../Components/Footer/Footer';
 import './Contact.css';
@@ -28,16 +27,6 @@ export default function Contact() {
             <h3 className='contact-subtitle'>Visit Us</h3>
             <span className='contact-text'>If you'd like to visit us in person, you can find us at the following location: KK Nurseries on Google Maps. </span>
             <iframe className='contact-branch' title='contact-branch' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3851.0206778430943!2d77.44261647365501!3d15.157209063210159!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bb6efc6d3de3bc3%3A0x452d751978a157c7!2sKK%20Nurseries%20(THIMMAPURAM%2CGUNTAKAL)!5e0!3m2!1sen!2sin!4v1697204318252!5m2!1sen!2sin"></iframe>
-        </div>
-        <div className='contact-info-div'>
-            <h3 className='contact-subtitle'>Connect With Us</h3>
-            <span className='contact-text'>Stay updated with our latest news and offers by connecting with us on social media platforms:</span>
-            <ul>
-                <li><Link  to="https://www.youtube.com/channel/UC3FvjGj2LVNb5sg9MUHMfYw" target="_blank">Youtube</Link></li>
-                <li><Link  to="https://api.whatsapp.com/send/?phone=%2B917386722020&text&type=phone_number&app_absent=0" target="_blank">Whatsapp</Link></li>
-                <li><Link  to="https://www.youtube.com/channel/UC3FvjGj2LVNb5sg9MUHMfYw" target="_blank">Instagram</Link></li>
-                <li><Link  to="https://www.youtube.com/channel/UC3FvjGj2LVNb5sg9MUHMfYw" target="_blank">Twitter</Link></li>
-            </ul>
         </div>
         <div className='contact-info-div'>
             <h3 className='contact-subtitle'>Have Questions?</h3>
