5d0029ec9027eaf2f98e9ec874c15a1b059ca485
Sai Dharshan <saidharshan018@gmail.com>
2023-10-19 10:37:28 +0530

    Some Home Page UI Updates

--- src/Components/Category/Category.js
+++ src/Components/Category/Category.js
@@ -1,51 +1,25 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React from 'react';
 import './Category.css';
 import { useNavigate } from 'react-router-dom';
 
 export default function Category(data) {
-
     const navigate = useNavigate();
-    const [scrollPosition, setScrollPosition] = useState(0);
-    const scrollDiv = useRef(null);
-
-    function handleScrollLeft() {
-        scrollDiv.current.scrollLeft -= 200;
-        setScrollPosition(scrollDiv.current.scrollLeft);
-    }
     
-    function handleScrollRight() {
-        scrollDiv.current.scrollLeft += 200;
-        setScrollPosition(scrollDiv.current.scrollLeft);
-    }
-
     function handelClick(title) {
         navigate('/listing/'+(title.toLowerCase()));
     }
-
-    useEffect(() => {
-        const handleScroll = () => {
-          setScrollPosition(scrollDiv.current.scrollLeft);
-        };
     
-        scrollDiv.current.addEventListener('scroll', handleScroll);
-        // return () => {
-        //   scrollDiv.current.removeEventListener('scroll', handleScroll);
-        // };
-      }, []);
-    
-
     return (
         <>
             <h1 className='category-title'>YOU CAN FIND</h1>
-            <div id='Category' className='category' ref={scrollDiv}>
-            {scrollPosition > 0 && <i className="fa-solid fa-circle-arrow-left" onClick={() => handleScrollLeft()}></i>}
+            <div className='you-can-find'>
             {data.data.length > 0 && data.data.map(item => (
-                <div key={item.id} className='category-item' onClick={() => handelClick(item.title)}>
-                    <img className='category-img' src={item.imgURL} loading='eager' alt={item.id} />
-                    <span className='category-name'>{item.title}</span>
+                <div key={item.id} className='category-item' style={{backgroundImage:`url(${item.imgURL})`}} onClick={() => handelClick(item.title)}>
+                    <div className='category-blank'>
+                    <h1 className='category-name'>{item.title}</h1>
+                    </div>
                 </div>
             ))}
-            <i className="fa-solid fa-circle-arrow-right" onClick={() => handleScrollRight()}></i>
             </div>
         </>
     )
