a9fe6da2766cf8baece3dcd22cb8acd49c079d4f
Sai Dharshan <saidharshan018@gmail.com>
2023-11-06 08:37:52 +0530

    Removed

--- src/Components/Nav/Navbar.js
+++ src/Components/Nav/Navbar.js
@@ -15,10 +15,6 @@ export default function Navbar(props) {
         setIsOpen(!isOpen);
     }
 
-    const profile = () => {
-        alert('Profile');
-    }
-
     function cartClick() {
         setCart(true);
     }
