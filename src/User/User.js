5c3512148d1399dd68fae969945ac20e02924cec
Sai Dharshan <saidharshan018@gmail.com>
2023-11-11 16:20:42 +0530

    User Page

--- src/User/User.js
+++ src/User/User.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './User.css';
 
 export default function User() {
+    const [user, setUser] = useState({
+        phone: null
+    });
+
+    const getOrderHistory = () => {
+        alert(user.phone)
+    }
+
     return (
-        <h1 className='user-title'>User</h1>
+        <>
+            <div className='user-main'>
+                <div className='user-body'>
+                <h1 className='user-title'>User Details</h1>
+                <div className='user-phone-div'>
+                <div className='user-phone-box'>
+                    <input className='user-phone' type='tel' name='phone' value={user.phone} onChange={(e) => setUser(preval => ({...preval, [e.target.name]:e.target.value}))} placeholder='Phone'/>
+                </div>
+                <button className='get-orders-history' onClick={() => getOrderHistory()}>Get Orders History</button>
+                </div>
+                </div>
+            </div>
+        </>
     )
 }
