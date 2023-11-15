8e168d93d44b0ec8d415d0d4dd2282270ffc23c5
Sai Dharshan <saidharshan018@gmail.com>
2023-11-15 14:32:49 +0530

    Got the order Details

--- src/User/User.js
+++ src/User/User.js
@@ -6,6 +6,7 @@ import './User.css';
 export default function User() {
     const [phone, setPhone] = useState('');
     const [loading, setLoading] = useState(false);
+    const [toggle, setToggle] = useState(false);
     const [orderList, setOrderList] = useState([]);
 
     const StyledCircularProgress = styled(CircularProgress) ({
@@ -26,8 +27,44 @@ export default function User() {
                 <h1 className='user-title'>Order Details</h1>
                 {(orderList && orderList.length > 0 && orderList.map(item => (
                     item.pay_type === 'Null' ? '' :
-                    <div className='order' key={item._id}>
+                    <div className='order' key={item.order_id}>
+                    <div className='order-head' onClick={() => setToggle(!toggle)}>
                         <h3 className='order_id'>{item.order_id}</h3>
+                        <span>{item.date}</span>
+                        {toggle ? <i className="fa-solid fa-caret-down"></i> :
+                        <i className="fa-solid fa-caret-right"></i>}
+                    </div>
+                    {toggle && <div className='order-body'>
+                        <h4>Plants Details</h4>
+                        {item.cartDate.map(item => (
+                        <div className='cart-item' key={item.key}>
+                            <img className='cart-item-img' src={item.imgURL} loading='lazy' alt='cart-img'/>
+                            <div className='cart-item-content'>
+                            <div className='cart-item-box-1'>
+                                <span className='cart-item-title'>{item.title}</span>
+                            </div>
+                            <div className='cart-item-box-2'>
+                                <span className='cart-item-price'>₹{item.price}</span>
+                                <div className='quantity-box'>
+                                <span className='quantity'>{item.count}</span>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        ))}
+                        <h4>User Details</h4>
+                        <div className='user-details'>
+                            <span className='user-name'>{item.name}</span>
+                            <span className='user-phone'>{item.phone}</span>
+                            <span className='user-email'>{item.email}</span>
+                            <span className='user-address'>{item.address}</span>
+                        </div>
+                        <h4>Payment Details</h4>
+                        <div className='payment-details'>
+                            <span className='payment-type'>{item.pay_type}</span>
+                            <span className='payment-status'>{item.pay_status}</span>
+                        </div>
+                    </div>}
                     </div>
                 ))) || 
                 <div className='user-note'>
