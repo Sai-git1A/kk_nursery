a0e7a153e7bd67cb02b2a22498915d63c1c776c0
Sai Dharshan <saidharshan018@gmail.com>
2023-11-15 11:10:52 +0530

    User Order Id

--- src/User/User.js
+++ src/User/User.js
@@ -1,25 +1,47 @@
 import React, {useState} from 'react';
+import axios from 'axios';
+import { CircularProgress, styled } from '@mui/material';
 import './User.css';
 
 export default function User() {
-    const [user, setUser] = useState({
-        phone: null
+    const [phone, setPhone] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [orderList, setOrderList] = useState([]);
+
+    const StyledCircularProgress = styled(CircularProgress) ({
+        color: '#4E944F'
     });
 
-    const getOrderHistory = () => {
-        alert(user.phone)
+    const getOrderHistory = async () => {
+        setLoading(true);
+        const resPIndoorPlants = await axios.get(`https://kknurseries-api.vercel.app/orders/${phone}`)
+        setOrderList(resPIndoorPlants.data);
+        setLoading(false);
     }
 
     return (
         <>
             <div className='user-main'>
                 <div className='user-body'>
-                <h1 className='user-title'>User Details</h1>
+                <h1 className='user-title'>Order Details</h1>
+                {(orderList && orderList.length > 0 && orderList.map(item => (
+                    item.pay_type === 'Null' ? '' :
+                    <div className='order' key={item._id}>
+                        <h3 className='order_id'>{item.order_id}</h3>
+                    </div>
+                ))) || 
+                <div className='user-note'>
+                <i className="fa-solid fa-clipboard-list order-list"></i>
+                <h3 className='order-list-text'>To access your order history, we require your phone number.</h3>
+                </div>}
+                {loading && <div className='circular-progress'>
+                    <StyledCircularProgress />
+                </div>}
                 <div className='user-phone-div'>
                 <div className='user-phone-box'>
-                    <input className='user-phone' type='tel' name='phone' value={user.phone} onChange={(e) => setUser(preval => ({...preval, [e.target.name]:e.target.value}))} placeholder='Phone'/>
+                    <input className='user-phone' type='tel' name='phone' value={phone} onChange={(e) => setPhone(e.target.value)} placeholder='Phone'/>
+                    <button className='get-orders-history' onClick={() => getOrderHistory()}><i className="fa-solid fa-magnifying-glass find"></i></button>
                 </div>
-                <button className='get-orders-history' onClick={() => getOrderHistory()}>Get Orders History</button>
                 </div>
                 </div>
             </div>
